326c5d3336a73c2789a8d571bdf401375f8866f1
1ucky40nc3 <louisnwendler@gmail.com>
2023-09-14 20:24:53 +0200

    added terraform configuration

---
+++ terraform/main.tf
@@ -0,0 +1,78 @@
+# This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
+# For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.82.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("key.json")
+
+  project = var.gcp_project
+  region  = var.gcp_region
+  zone    = var.gcp_zone
+}
+
+resource "google_compute_instance" "prollm-translator" {
+  boot_disk {
+    auto_delete = true
+    device_name = var.google_compute_instance_name
+
+    initialize_params {
+      image = var.google_compute_instance_image
+      size  = 10
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+    # Install Ops Agent for Monitoring and Logging
+    goog-ops-agent-policy = "v2-x86-template-1-1-0"
+  }
+
+  machine_type = "e2-micro"
+
+  metadata = {
+    enable-osconfig = "TRUE"
+    # Add a manually generated SSH key
+    ssh-keys = "${var.ssh_username}:${file("../.ssh/id_rsa.pub")}"
+  }
+
+  name = var.google_compute_instance_name
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    # Select the default subnetwork the project's region
+    subnetwork = "projects/${var.gcp_project}/regions/${var.gcp_region}/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = true
+    enable_vtpm                 = true
+  }
+
+  zone = var.gcp_zone
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,41 @@
+variable "gcp_project" {
+  description = "A GCP project"
+  type        = string
+  default     = "prollm-translator"
+}
+
+variable "gcp_region" {
+  description = "A GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "gcp_zone" {
+  description = "A GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "google_compute_instance_name" {
+  description = "The name of the prollm-translator compute instance"
+  type        = string
+  default     = "prollm-translator"
+}
+
+variable "google_compute_instance_image" {
+  description = "A Google Compute Instance image"
+  type        = string
+  default     = "projects/debian-cloud/global/images/debian-11-bullseye-v20230912"
+}
+
+variable "ssh_key_user" {
+  description = "The user of the manually generated SSH key"
+  type        = string
+  default     = "louis"
+}
+
+variable "ssh_username" {
+  description = "The username of the manually generated SSH key"
+  type        = string
+  default     = "louis"
+}
